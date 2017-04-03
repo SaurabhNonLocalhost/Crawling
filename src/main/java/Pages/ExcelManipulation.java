0291b1afc2bf5551d0efdabf91ff21171ccb7a12
Saurabh <saurabhkumar202@yahoo.com>
2017-04-03 06:50:52 +0530

    Changes to correct crawling algorithm.

--- src/main/java/Pages/ExcelManipulation.java
+++ src/main/java/Pages/ExcelManipulation.java
@@ -1,3 +1,84 @@
+<<<<<<< HEAD
+package Pages;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Iterator;
+
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.xssf.usermodel.XSSFCell;
+import org.apache.poi.xssf.usermodel.XSSFRow;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+public class ExcelManipulation {
+	XSSFSheet sheet;
+	XSSFWorkbook workbook;
+	int rowCount;
+	int columnCount;
+	FileOutputStream outputStream;
+	FileInputStream inputStream;
+
+	public ExcelManipulation() {
+		workbook = new XSSFWorkbook();
+		sheet = workbook.createSheet("LearningExchangeData");
+		rowCount = 0;
+		columnCount = 0;
+	}
+
+	public void excelWrite(String data) throws IOException {
+		XSSFRow row = sheet.createRow(++rowCount);
+		XSSFCell cell = row.createCell(2);
+		cell.setCellValue(data);
+		//cell.setCellValue(data);
+
+		try {
+			outputStream = new FileOutputStream("LearningExchangeData.xlsx");
+			workbook.write(outputStream);
+			outputStream.close();
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public ArrayList<String> excelRead() throws IOException {
+
+		String excelFilePath = "LearningExchangeData.xlsx";
+		inputStream = new FileInputStream(new File(excelFilePath));
+		String cellvalue = null;
+		XSSFWorkbook workbook = new XSSFWorkbook(inputStream);
+		XSSFSheet firstSheet = workbook.getSheetAt(0);
+		Iterator<Row> iterator = firstSheet.iterator();
+		ArrayList<String> a = new ArrayList<String>();
+		
+		while (iterator.hasNext()) {
+			Row nextRow = iterator.next();
+			Cell cell = nextRow.getCell(2);
+			cellvalue = cell.getStringCellValue();
+			a.add(cellvalue);
+		}
+
+		workbook.close();
+		
+		return a;
+
+		
+	}
+	
+	public void destroy() throws IOException
+	{
+		outputStream.close();
+		inputStream.close();
+	}
+
+=======
 package Pages;
 
 import java.io.FileNotFoundException;
@@ -41,4 +122,5 @@ public class ExcelManipulation {
          e.printStackTrace();
      }
 }
+>>>>>>> origin/master
 }
--- src/main/java/Pages/GetURLs.java
+++ src/main/java/Pages/GetURLs.java
@@ -1,3 +1,216 @@
+<<<<<<< HEAD
+package Pages;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.ListIterator;
+import java.util.concurrent.TimeUnit;
+
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.By;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+
+public class GetURLs {
+	WebDriver driver;
+	ExcelManipulation excelobj;
+
+
+	
+	public GetURLs() {
+		System.setProperty("webdriver.chrome.driver", "D:\\Software Testing\\chromedriver.exe");
+		
+		//System.setProperty("webdriver.ie.driver", "D:\\Software Testing\\IEDriverServer.exe");
+		//System.setProperty("webdriver.gecko.driver", "C://Users//Saurabh//Desktop//Personal//ff//geckodriver.exe");
+		driver=new ChromeDriver();
+		//driver = new InternetExplorerDriver();
+		driver.manage().window().maximize();
+		excelobj = new ExcelManipulation();
+
+	}
+
+	public List<String> extractLinks(String url) throws IOException {
+
+		ArrayList<String> result = new ArrayList<String>();
+		result.clear();
+
+		// Document doc = Jsoup.connect(url).get();
+
+		driver.get(url);
+		driver.manage().timeouts().implicitlyWait(300, TimeUnit.SECONDS);
+
+		// Elements links = doc.select("a[href]");
+		// Elements media = doc.select("[src]");
+		// Elements imports = doc.select("link[href]");
+
+		// href ...
+		// if (!links.isEmpty()) {
+		// for (Element link : links) {
+		// result.add(link.attr("abs:href"));
+		// }
+
+		// }
+
+		// img ...
+		/*
+		 * for (Element src : media) { result.add(src.attr("abs:src")); }
+		 * 
+		 * // js, css, ... for (Element link : imports) {
+		 * result.add(link.attr("abs:href")); }
+		 */
+		List<WebElement> myLinks = null;
+		boolean done = false;
+		int attempts = 0;
+		while (attempts < 5) {
+			try {
+				myLinks = driver.findElements(By.tagName("a"));
+				if (!myLinks.isEmpty()) {
+					for (WebElement link : myLinks) {
+						//System.out.println("Current link href attribute is: "+link.getAttribute("href"));
+						if ((link.getAttribute("href"))!=null && (link.getAttribute("href")).contains("learningexchange") )
+							result.add(link.getAttribute("href"));
+					}
+				}
+				done = true;
+				break;
+			} catch (Exception e) {
+				System.out.println("Exception caught is : -");
+				e.printStackTrace();
+				result.clear();
+			}
+			attempts++;
+		}
+		return result;
+
+	}
+
+
+	public void navigateAndAdd() throws Exception {
+		try {
+			//GetURLs obj = new GetURLs();
+			// ExcelManipulation excelobj = new ExcelManipulation();
+			String parentURL = "https://learningexchange.ptc.com/";
+			List<String> mainList = extractLinks(parentURL);
+			if (!mainList.isEmpty()) {
+				// ExcelManipulation excelobj2 = new ExcelManipulation();
+				List<String> sub = new ArrayList<String>();
+				//ListIterator<String> parentIterator = mainList.listIterator();
+				// ListIterator<String> parentIterator2 =
+				// mainList.listIterator();
+				int i;
+
+				for (int j = 0; j < mainList.size(); j++) {
+					String current = mainList.get(j);
+					System.out.println("Current list of elements are ");
+					for (int k = 0; k < mainList.size(); k++) {
+						//int count=k+1;
+						System.out.println( mainList.get(k));
+						//excelobj.excelWrite(mainList.get(k));
+					}
+
+					if (current.contains("learningexchange")) {
+
+						System.out.println("Traversing Parent link" + current);
+						excelobj.excelWrite("------Parent link is----- " + current);
+						sub = extractLinks(current);
+
+						ListIterator<String> childIterator = sub.listIterator();
+						while (childIterator.hasNext()) {
+							String child = childIterator.next();
+							if (!mainList.contains(child) && child.contains("learningexchange")) {
+
+								System.out.println("Adding this new link " + child);
+								excelobj.excelWrite(child);
+
+								// parentIterator.add(child);
+								mainList.add(mainList.size() - 1, child);
+
+							}
+						}
+					}
+					System.out.println("CRAWLING IS OVER FOR CURRENT PARENT LINK: " + current);
+
+				}
+				System.out.println("CRAWLING IS OVER FOR ALL THE LINKS");
+				// testOtherThan200();
+
+			}
+		}
+
+		catch (Exception e) {
+			driver.quit();
+			e.printStackTrace();
+		}
+	}
+
+
+	public void testOtherThan200() {
+		try {
+
+			ArrayList<String> URLsToVerify = excelobj.excelRead();
+			// String URLToVerify = "https://mail.google.com";
+
+			for (int i = 0; i < URLsToVerify.size(); i++) {
+				String current = URLsToVerify.get(i);
+				URL url = new URL(current);
+				HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+				conn.setRequestMethod("GET");
+				conn.connect();
+				// conn.setRequestProperty(“Accept”, “application/json”);l
+
+				if (conn.getResponseCode() != 200) {
+					throw new RuntimeException("HTTP error code:" + conn.getResponseCode());
+				} else {
+
+					System.out.println("The response code is" + conn.getResponseCode());
+
+					String currentDir = System.getProperty("user.dir");
+
+					File src = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+					FileUtils.copyFile(src, new File(currentDir + "\\screenshots\\" + Math.random() + ".png"));
+
+				}
+
+				driver.quit();
+			}
+
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			driver.quit();
+			e.printStackTrace();
+		}
+		
+
+	}
+	
+	public static void main(String[] args) throws Exception
+	{
+		GetURLs obj = new GetURLs();
+		obj.navigateAndAdd();
+		//obj.testOtherThan200();
+		//obj.last();
+	}
+	
+	
+	public void last()
+	{
+		try {
+			excelobj.destroy();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+}
+=======
 package Pages;
 
 import java.io.IOException;
@@ -122,3 +335,4 @@ public class GetURLs {
 	}
 
 }
+>>>>>>> origin/master
